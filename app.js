e160fa5132ea93b8da27cb487f39f256ef5c71ee
Colton <colton_rusch@brown.edu>
2022-09-10 17:03:34 -0400

    finished please check us off please check off us

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const apiRequest = async () => {
    */
 
   // TODO fill in your own port number 
-  const PORT_NUMBER = "";
+  const PORT_NUMBER = "8010";
 
   const baseUrl = `http://localhost:${PORT_NUMBER}/proxy/api/`
 
@@ -29,7 +29,7 @@ const apiRequest = async () => {
     }
   });
 
-  // console.log(response);
+  console.log(response);
 
   // Return the response in JSON format
   return response.json();
@@ -39,16 +39,21 @@ const updatePage = async () => {
   const gallery = document.getElementById('cs1300-gallery');
 
   // Make API request and get an array of fruit objects
-  const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  let fruitsArray = await apiRequest();
+  console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15", 
+  fruitsArray = fruitsArray.filter(fruit => fruit.nutritions.sugar > 15);
 
-  // TODO: Create a new HTML element to display your data 
+  // TODO: Create a new HTML element to display your data
+  // Create a new HTML element and set its properties
+  const newElement = document.createElement('div');
+  newElement.innerHTML = fruitsArray.map(fruit => fruit.name).join(", ");
 
   // TODO: Append your new element to the page
-
+  const existingElement = document.getElementById('fruit');
+  existingElement.append(newElement);
 }
 
 // SAMPLE CODE of how to create and append a new HTML element to the page
